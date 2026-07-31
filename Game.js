159b8d9d97338295fa3076eea3f41be532282c02
Athar Ashraf War <atherr977@gmail.com>
2026-07-31 09:45:34 +0530

    Update Game.js

--- Game.js
+++ Game.js
@@ -39,7 +39,7 @@ function levelUp() {
   level++;
   h2.innerText = `Level ${level}`;
 
-  let randIdx = Math.floor(Math.random() * 3);
+  let randIdx = Math.floor(Math.random() * 4);
   let randColor = btns[randIdx];
   let randBtn = document.querySelector(`.${randColor}`);
   gameSeq.push(randColor);
