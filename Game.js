c5e343717754f81135c310f5dfe92a1b1f398efa
Athar Ashraf War <atherr977@gmail.com>
2026-07-31 10:32:07 +0530

    Update Game.js

--- Game.js
+++ Game.js
@@ -86,7 +86,7 @@ async function showRandomFact() {
   factElement.innerHTML = "Loading a random fact...";
 
   try {
-    let response = await fetch("https://restcountries.com/v3.1/all");
+    let response = await fetch("https://api.api-ninjas.com/v1/facts");
     let data = await response.json();
 
     factElement.innerHTML = `💡 <strong>Random Fact:</strong> ${data.text}`;
